c17bd29afe463b253b59fe8108457106546a93aa
monattv <stefanogiaccone.email@gmail.com>
2026-08-03 13:28:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ const CASA = {
 
     sottotitolo: "DESCRIZIONE",
 
-    telefono: "+39XXXXXXXXXX",
+    telefono: "+393388727684",
 
-    whatsapp: "39XXXXXXXXXX",
+    whatsapp: "+393388727684",
 
-    booking: "LINK_BOOKING",
+    booking: "https://www.booking.com/hotel/it/la-casetta-terrasini.it.html",
 
     instagram: "LINK_SOCIAL",
 
-    indirizzo: "INDIRIZZO",
+    indirizzo: "Via Ruggero Settimo, n72, 90049 Terrasini PA",
 
-    maps: "LINK_MAPS",
+    maps: "https://www.google.com/maps/place/la+casetta+Terrasini/@38.1510133,13.0850061,17z/data=!3m1!4b1!4m6!3m5!1s0x13199336f3c892c3:0x121b13ab77110af7!8m2!3d38.1510133!4d13.0850061!16s%2Fg%2F11n1xcvvjj?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D",
 
     email: "EMAIL"
 
