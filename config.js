725801c562551e338e44c5540bf45b8dea5635cf
monattv <stefanogiaccone.email@gmail.com>
2026-08-03 13:19:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
+const CASA = {
 
+    nome: "NOME CASA",
+
+    sottotitolo: "DESCRIZIONE",
+
+    telefono: "+39XXXXXXXXXX",
+
+    whatsapp: "39XXXXXXXXXX",
+
+    booking: "LINK_BOOKING",
+
+    instagram: "LINK_SOCIAL",
+
+    indirizzo: "INDIRIZZO",
+
+    maps: "LINK_MAPS",
+
+    email: "EMAIL"
+
+};
